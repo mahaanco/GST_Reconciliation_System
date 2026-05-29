3fc19983a9023dbb52ffbf6b14f2066bcad475d0
mahaanco <office@camaha.co.in>
2026-05-29 13:16:59 +0530

    Update csv_parser.py

--- parsers/csv_parser.py
+++ parsers/csv_parser.py
@@ -6,7 +6,26 @@ class CSVParser:
     @staticmethod
     def read(file):
 
-        return pd.read_csv(
-            file,
-            encoding_errors="ignore"
+        encodings = [
+            "utf-8",
+            "latin-1",
+            "cp1252"
+        ]
+
+        for encoding in encodings:
+
+            try:
+
+                file.seek(0)
+
+                return pd.read_csv(
+                    file,
+                    encoding=encoding
+                )
+
+            except Exception:
+                pass
+
+        raise ValueError(
+            "Unable to read CSV file"
         )
